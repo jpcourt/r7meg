5d2d4549a0e69874df45c90f26dd839fc4169182
unknown <jpcourt@FRLUMLPT0016840.cpgrp.root.local>
2014-07-28 10:46:55 +0200

    Mise en dynamique de la gestion des vecteurs

--- js/productManager.js
+++ js/productManager.js
@@ -1,24 +1,50 @@
+var offerConfig;
 var products;
 var typeMa;
+//var vector;
 
-function displayProducts(vector){
+function initialize(){
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'offers/offerConfig.json');
 	xhr.onreadystatechange = function(aEvt) {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
-				var offerConfig = JSON.parse(xhr.response);
-				offerConfig['offerList'].forEach(function(offerItem){
-					if(offerItem['vector'] == vector){
-						products = offerItem['productList'];
-						typeMa = offerItem['typeMa'];
-					}
-				});
-				updateProducts(vector);
+				offerConfig = JSON.parse(xhr.response);
+				displayVectorSelector();
+				displayInterface(offerConfig['offerList'][0]['vector']);
 			}
 		}
 	}
 	xhr.send();
+}
+
+function displayVectorSelector(){
+	var selectorButton = "<button type='button' class='btn btn-default dropdown-toggle' data-toggle='dropdown'>";
+	selectorButton += "<p id='selectorName'></p><span class='caret'></span></button>";
+	selectorButton += "<ul class='dropdown-menu text-center' role='menu'>";
+	offerConfig['offerList'].forEach(function(offer){
+		selectorButton += "<li onclick='displayInterface("+JSON.stringify(offer['vector']+")'><a href='#'>TNT</a></li>";
+	})				
+	selectorButton += "</ul>";
+	document.getElementById('vectorSelector').innerHTML = selectorButton;
+}
+
+function displayInterface(vector){	
+	displayProducts(vector);
+	updateGenerateButton(vector);
+	document.getElementById('selectorName').innerHTML = 'Vecteur : '+vector;
+	document.getElementById('products').value = "";
+	document.getElementById('megResult').innerHTML = "\n";
+}
+
+function displayProducts(vector){
+	offerConfig['offerList'].forEach(function(offerItem){
+		if(offerItem['vector'] == vector){
+			products = offerItem['productList'];
+			typeMa = offerItem['typeMa'];
+		}
+	});
+	updateProducts(vector);
 }
 
 function updateProducts(vector){
@@ -31,20 +57,13 @@ function updateProducts(vector){
 	productDisplay += "</div>";
 	productDisplay += "<div><button class='btn btn-default' onclick='resetProducts("+JSON.stringify(vector)+")'>Reset</button></div>";
 	document.getElementById('productList').innerHTML = '<div>'+productDisplay+'</div>';
+}
 
+function updateGenerateButton(vector){
 	var buttonDisplay = "<button class='btn btn-success btn-lg' type='submit' ";
 	buttonDisplay += "onclick='generateMeg("+JSON.stringify(vector)+", "+typeMa+"), displaySelectedProducts()'>"; 
 	buttonDisplay += "Générer MEG "+vector+"</button>";
 	document.getElementById('generateButton').innerHTML = buttonDisplay;
-}
-
-function displayInterface(vector){
-	
-	displayProducts(vector);
-
-	document.getElementById('selectorName').innerHTML = 'Vecteur : '+vector;
-	document.getElementById('products').value = "";
-	document.getElementById('megResult').innerHTML = "\n";
 }
 
 function selectedProductList(){
